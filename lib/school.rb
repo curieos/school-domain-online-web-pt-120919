26ad156ba8df040db83878595ec8b86658940474
Caroline Longnecker <cvrieous@gmail.com>
2019-12-24 04:04:22 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,7 +19,7 @@ class School
   end
   
   def sort
-    roster.each do |grade, students|
+    roster.collect do |grade, students|
       students = students.sort
       { grade: students }
     end
